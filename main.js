4534438f9a845cfa6ec10d69670c6d000ce5cfa2
sohagraha <sohagrahabd@gmail.com>
2021-09-02 13:34:35 +0600

    added

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 document.getElementById('searchBook').addEventListener('click', function () {
     const inputValue = document.getElementById('searchTxt').value;
-    // document.getElementById('searchTxt').value = '';
     getBook(inputValue);
 });
+
+document.getElementById('searchTxt').value = 'somaj';
 
 const getBook = searchText => {
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
@@ -12,47 +13,54 @@ const getBook = searchText => {
 }
 
 const showBook = books => {
-    const bookList = books.docs;
     console.log(books.numFound);
-
-
+    const bookList = books.docs;
 
     bookList.forEach(book => {
-        // console.log(book.cover_i);
-        const div = document.createElement('div');
-        if (book.cover_i === undefined) {
-            div.innerHTML = `
-                <div class="col"  onclick="clicked('${book.cover_i}')">
-                    <div class="card h-100">
-                            <img src="images/no-image.jpg" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><span class="fw-bold">Book Name</span> : ${book.title}</h5>
-                            <p class="card-text text-center"><span class="fw-bold">Authors</span> : ${book.author_name}</p>
-                            <p class="card-text text-center"><span class="fw-bold">First Publish :</span> : ${book.publish_date[book.edition_count - 1]}</p>
-                        </div>
-                    </div>
-                </div>
-        `
+
+        // publish year available or not condition 
+        let date;
+        if (book.first_publish_year === undefined) {
+            date = 'Unknown'
         }
         else {
-            div.innerHTML = `
-                <div class="col"  onclick="clicked('${book.cover_i}')">
-                    <div class="card h-100">
-                            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top img-fluid" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title text-center"><span class="fw-bold">Book Name</span> : ${book.title}</h5>
-                            <p class="card-text text-center"><span class="fw-bold">Authors</span> : ${book.author_name}</p>
-                            <p class="card-text text-center"><span class="fw-bold">First Publish :</span> ${book.publish_date[book.edition_count - 1]}</p>
-                        </div>
-                    </div>
-                </div>
-        `
+            date = book.first_publish_year;
         }
-        document.getElementById('bookContainer').appendChild(div)
-    });
 
+        // image available or not condition 
+        let image;
+        if (book.cover_i === undefined) {
+            image = "images/no-image.jpg";
+        }
+        else {
+            image = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        }
+
+
+        if (books.numFound === 0) {
+            let p = document.createElement('p');
+            p.innerHTML = `
+                <p>No </p>
+            `
+        }
+
+
+        let div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `<div class="card h-100" onclick="bookDetail()">
+                            <img src=${image} class="h-75" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title text-center"><span class="fw-bold">Title</span> : ${book.title}</h5>
+                                <p class="card-text text-center"><span class="fw-bold">Authors</span> : ${book.author_name}</p>
+                                <p class="card-text text-center"><span class="fw-bold">First Publish</span> : ${date}</p>
+                            </div>
+                        </div>`
+        document.getElementById('bookContainer').appendChild(div);
+    });
 }
 
-const clicked = id => {
-    console.log(id);
+
+
+const bookDetail = () => {
+    console.log("id");
 }
